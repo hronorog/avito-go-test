6f58466053cdeff0d8668004d9276f9cc22d01e9
maritajo <maritajo@school21.ru>
2026-03-23 17:52:59 +0000

    dev: add dummylogin

--- internal/httpserver/router.go
+++ internal/httpserver/router.go
@@ -1,6 +1,22 @@
 package httpserver
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type DummyLoginRequest struct {
+	Login string `json:"login"`
+	Role  string `json:"role"`
+}
+
+type DummyLoginResponse struct {
+	Token string `json:"token"`
+}
 
 func New() http.Handler {
 	mux := http.NewServeMux()
@@ -10,5 +26,47 @@ func New() http.Handler {
 		w.Write([]byte(`{"status":"ok"}`))
 	})
 
+	mux.HandleFunc("/dummyLogin", dummyLoginHandler)
+
 	return mux
+}
+
+func dummyLoginHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	var req DummyLoginRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, "bad request", http.StatusBadRequest)
+		return
+	}
+
+	//TODO: подключение к бд потом
+	userID := int64(1)
+
+	secret := os.Getenv("JWT_SECRET")
+	if secret == "" {
+		secret = "dev-secret"
+	}
+
+	claims := jwt.MapClaims{
+		"user_id": userID,
+		"role":    req.Role,
+		"login":   req.Login,
+		"exp":     time.Now().Add(24 * time.Hour).Unix(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	signed, err := token.SignedString([]byte(secret))
+	if err != nil {
+		http.Error(w, "internal error", http.StatusInternalServerError)
+		return
+	}
+
+	resp := DummyLoginResponse{Token: signed}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	_ = json.NewEncoder(w).Encode(resp)
 }
