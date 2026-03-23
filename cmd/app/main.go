55e350b9ffdd273594820afe576d78a548112fda
maritajo <maritajo@school21.ru>
2026-03-23 18:56:45 +0000

    dev: add postgres

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/lib/pq"
 
 	"github.com/hronorog/avito-go-test/internal/httpserver"
 )
@@ -14,7 +17,22 @@ func main() {
 		port = "8080"
 	}
 
-	handler := httpserver.New()
+	dsn := os.Getenv("DB_DNS")
+	id dns == "" {
+		dns = "host=localhost port=5432 user=postgres password=postgres dbname=avito_rooms sslmode=disable"
+	}
+
+	db, err := aql.Open("postgress", dsn)
+	if err != nil (
+		log.Fatal("sql.Open:", err)
+	)
+	defer db.Close()
+
+	if err := db.Ping(); err != nil {
+		log.Fatal("db.Ping:", err)
+	}
+
+	handler := httpserver.New(db)
 
 	log.Println("listening on :%s", port)
 	if err := http.ListenAndServe(":"+port, handler); err != nil {
--- go.mod
+++ go.mod
@@ -2,4 +2,6 @@ module github.com/hronorog/avito-go-test
 
 go 1.22.2
 
-require github.com/golang-jwt/jwt/v5 v5.3.1 
+require github.com/golang-jwt/jwt/v5 v5.3.1
+
+require github.com/lib/pq v1.12.0 // indirect
